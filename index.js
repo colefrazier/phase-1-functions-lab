98ed7cc6699f3cf0e695c15e37ff677eac0375e5
colefrazier <cfrazier541@gmail.com>
2022-10-11 17:07:53 -0400

    commit tree b1327d92fe62eb4258a396efd4cccb26784afe02

--- index.js
+++ index.js
@@ -1,1 +1,30 @@
-// Code your solution in this file!
+ function distanceFromHqInBlocks(customerLoaction) {
+   customerLoaction = customerLoaction - 42;
+   return Math.abs(customerLoaction)
+}
+
+
+function distanceFromHqInFeet(customerLoaction) {
+    distanceFromHqInBlocks(customerLoaction);
+    return distanceFromHqInBlocks(customerLoaction) * 264;
+
+}
+
+function distanceTravelledInFeet(start, destination) {
+    let feetTravelled = (destination - start) * 264;
+    return Math.abs(feetTravelled);
+
+}
+
+function calculatesFarePrice(start, destination) {
+    let feetTravelled = Math.abs((destination - start) * 264);
+    if ( feetTravelled <= 400) {
+        return 0;
+    } else if (feetTravelled > 400 && feetTravelled <= 2000) {
+        return .02 * (feetTravelled - 400);
+    } else if (feetTravelled > 2000 && feetTravelled < 2500) {
+        return 25;
+    } else {
+        return 'cannot travel that far';
+    }
+}
